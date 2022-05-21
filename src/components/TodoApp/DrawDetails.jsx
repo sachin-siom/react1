12cce15e7fd6e14f0b4bf53f3f39a7cf92691348
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-05-21 21:43:07 +0530

    draw detail and retailer detail table wonPoints bgColor Change

--- src/components/TodoApp/DrawDetails.jsx
+++ src/components/TodoApp/DrawDetails.jsx
@@ -2,13 +2,20 @@ import React, { Component, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import AuthenticationService from "./AuthenticationService.js";
-import { DataGrid,GridToolbarContainer, GridToolbarExport } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridToolbarContainer,
+  GridToolbarExport,
+} from "@mui/x-data-grid";
+
+import clsx from "clsx";
 import { drawDetailsUrl } from "./Constant";
 import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import RetailerDropDown from "./RetailerDropDown.jsx";
 import {
+  Box,
   Button,
   Card,
   CardContent,
@@ -48,24 +55,81 @@ class DrawDetails extends React.Component {
 
   render() {
     const columns = [
-      { field: "id", headerName: "ID", width: 150 , headerAlign: 'center', align: 'center'},
-      { field: "draw", headerName: "Draw Time", width: 150 , headerAlign: 'center', align: 'center'},
-      { field: "setPoints", headerName: "Set Points", width: 150 , headerAlign: 'center', align: 'center'},
-      { field: "wonPoints", headerName: "Won Points", width: 150 , headerAlign: 'center', align: 'center'},
-      { field: "betCount", headerName: "Total Bet Count", width: 150 , headerAlign: 'center', align: 'center'},
-      { field: "winCount", headerName: "Bet Won Count", width: 150 , headerAlign: 'center', align: 'center'},
-      { field: "winNumbers", headerName: "Winning Numbers", width: 150 , headerAlign: 'center', align: 'center'},
+      {
+        field: "id",
+        headerName: "ID",
+        width: 150,
+        headerAlign: "center",
+        align: "center",
+      },
+      {
+        field: "draw",
+        headerName: "Draw Time",
+        width: 150,
+        headerAlign: "center",
+        align: "center",
+      },
+      {
+        field: "setPoints",
+        headerName: "Set Points",
+        width: 150,
+        headerAlign: "center",
+        align: "center",
+      },
+      {
+        field: "wonPoints",
+        headerName: "Won Points",
+        cellClassName: (params) => {
+          if (params.value == null) {
+            return "";
+          }
+
+          return clsx("super-app", {
+            positive: params.value > 0,
+          });
+        },
+        width: 150,
+        headerAlign: "center",
+        align: "center",
+      },
+      {
+        field: "betCount",
+        headerName: "Total Bet Count",
+        width: 150,
+        headerAlign: "center",
+        align: "center",
+      },
+      {
+        field: "winCount",
+        headerName: "Bet Won Count",
+        width: 150,
+        headerAlign: "center",
+        align: "center",
+      },
+      {
+        field: "winNumbers",
+        headerName: "Winning Numbers",
+        width: 250,
+        headerAlign: "center",
+        align: "center",
+      },
     ];
-    
-    const propsRowStyle = (rowData, index)=>({
-      backgroundColor: rowData.wonPoints> 0 ? '#90EE90' : '#fff',
-    })
-    
-    
+    const datagridSx = {
+      borderRadius: 2,
+      "& .MuiDataGrid-main": { borderRadius: 2 },
+      "& .MuiDataGrid-virtualScrollerRenderZone": {
+        "& .MuiDataGrid-row": {
+          "&:nth-child(2n)": { backgroundColor: "rgba(235, 235, 235, .7)" },
+        },
+      },
+      "& .MuiDataGrid-columnHeaders": {
+        fontSize: 16,
+      },
+    };
     return (
       <div>
         <center>
-        <Card style={{ width: "95%", top:0 }} >
+          <Card style={{ width: "95%", top: 0 }}>
             <CardHeader
               // subheader="Manage "
               title="Draw Details"
@@ -83,51 +147,63 @@ class DrawDetails extends React.Component {
                   // }}
                   xs={20}
                 >
-        <div className="col-sm-10">
-          <RetailerDropDown parentCallback={this.handleCallback} />
-        </div>
-        <div className="col-sm-10">
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <DesktopDatePicker
-              label="Draw Date"
-              inputFormat="yyyy-MM-dd"
-              value={this.state.startDate}
-              onChange={this.handleChange}
-              renderInput={(params) => <TextField {...params} />}
-            />
-          </LocalizationProvider>
-        </div>
-        <div className="col-sm-10">
-          <Button
-            style={{ margin: 8 }}
-            onClick={this.submitData}
-            variant="contained"
-            color="success"
-          >
-            Get Draw Details
-          </Button>
-        </div>
-        <div
-          style={{
-            height: 400,
-            width: "100%",
-            textAlign: "center",
-          }}
-        >
-          <DataGrid
-            style={{ display: "flex" }}
-            rows={this.state.data}
-            rowStyle={propsRowStyle}
-            columns={columns}
-            pageSize={100}
-            components={{ Toolbar: this.CustomToolbar }}
-          />
-        </div>
-        </Grid>
-      </Grid>
-      </CardContent>
-      </Card>
-      </center>
+                  <div className="col-sm-10">
+                    <RetailerDropDown parentCallback={this.handleCallback} />
+                  </div>
+                  <div className="col-sm-10">
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                      <DesktopDatePicker
+                        label="Draw Date"
+                        inputFormat="yyyy-MM-dd"
+                        value={this.state.startDate}
+                        onChange={this.handleChange}
+                        renderInput={(params) => <TextField {...params} />}
+                      />
+                    </LocalizationProvider>
+                  </div>
+                  <div className="col-sm-10">
+                    <Button
+                      style={{ margin: 8 }}
+                      onClick={this.submitData}
+                      variant="contained"
+                      color="success"
+                    >
+                      Get Draw Details
+                    </Button>
+                  </div>
+                  <div
+                    style={{
+                      height: 700,
+                      width: "100%",
+                      textAlign: "center",
+                    }}
+                  >
+                    <Box
+                      sx={{
+                        height: 700,
+                        width: 1,
+                        "& .super-app.positive": {
+                          backgroundColor: "rgba(157, 255, 118, 0.49)",
+                          color: "#1a3e72",
+                          fontWeight: "600",
+                        },
+                      }}
+                    >
+                      <DataGrid
+                        style={{ display: "flex" }}
+                        rows={this.state.data}
+                        columns={columns}
+                        sx={datagridSx}
+                        pageSize={100}
+                        components={{ Toolbar: this.CustomToolbar }}
+                      />
+                    </Box>
+                  </div>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </center>
       </div>
     );
   }
