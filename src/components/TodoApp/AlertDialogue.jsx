52a05cbc7f5fe9bc6e73d969d4fc870453b1d515
sachinsan <82144151+sachin-siom@users.noreply.github.com>
2022-11-26 23:43:38 +0530

    Update AlertDialogue.jsx
    table problem is fixed

--- src/components/TodoApp/AlertDialogue.jsx
+++ src/components/TodoApp/AlertDialogue.jsx
@@ -1,20 +1,11 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import Avatar from '@mui/material/Avatar';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemText from '@mui/material/ListItemText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
-import PersonIcon from '@mui/icons-material/Person';
-import AddIcon from '@mui/icons-material/Add';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import TextareaAutosize from '@mui/material/TextareaAutosize';
 
 
-const emails = ['username@gmail.com', 'user02@gmail.com'];
 
 export interface SimpleDialogProps {
     open: boolean;
@@ -23,10 +14,17 @@ export interface SimpleDialogProps {
 
 function SimpleDialog(props: SimpleDialogProps) {
     const { onClose, open, ticketData } = props;
+    if(!open) return null;
     const obj = JSON.parse(JSON.stringify(ticketData));
-    var data = JSON.parse(obj.pointDetails.points).map((details, i) => (
+    var unflattenData = JSON.parse(obj.pointDetails.points).map((details, i) => (
         Object.entries(details.points).sort().map(([k, v]) => `${k} | ${v*details.winningMultiplier} `)
     ))
+
+    var flattenData = unflattenData.flat(1)
+    
+    var finalData = [];
+    for (var i=0,len=flattenData.length; i<len; i+=3)
+        finalData.push(flattenData.slice(i,i+3));
     
     const handleClose = () => {
         onClose();
@@ -71,30 +69,29 @@ function SimpleDialog(props: SimpleDialogProps) {
                 noValidate
                 autoComplete="off"
             >
-                {/*                 <TextareaAutosize
-                    variant='outlined'
-                    label="Bet No"
-                    aria-label="Ticket No"
-                    placeholder="Ticket No"
-                    value={data}
-                /> */}
                 <table style={{ "borderWidth": "1px", 'borderColor': "#aaaaaa", 'borderStyle': 'solid' }}>
                     <tr style={{ "borderWidth": "1px", 'borderColor': "#aaaaaa", 'borderStyle': 'solid' }}>
                     <th style={{"borderWidth":"1px", 'borderColor':"#aaaaaa", 'borderStyle':'solid', 'textAlign':'center'}}>Bet No</th>
                     <th style={{"borderWidth":"1px", 'borderColor':"#aaaaaa", 'borderStyle':'solid', 'textAlign': 'center' }}>Qty</th>
-                        {/* <th style={{"borderWidth":"1px", 'borderColor':"#aaaaaa", 'borderStyle':'solid', 'textAlign':'center'}}>Column 2</th>
-                        <th style={{"borderWidth":"1px", 'borderColor':"#aaaaaa", 'borderStyle':'solid', 'textAlign':'center'}}>Column 3</th> */}
+                    <th style={{"borderWidth":"1px", 'borderColor':"#aaaaaa", 'borderStyle':'solid', 'textAlign':'center'}}>Bet No</th>
+                    <th style={{"borderWidth":"1px", 'borderColor':"#aaaaaa", 'borderStyle':'solid', 'textAlign': 'center' }}>Qty</th>
+                    <th style={{"borderWidth":"1px", 'borderColor':"#aaaaaa", 'borderStyle':'solid', 'textAlign':'center'}}>Bet No</th>
+                    <th style={{"borderWidth":"1px", 'borderColor':"#aaaaaa", 'borderStyle':'solid', 'textAlign': 'center' }}>Qty</th>
                     </tr>
-                    {data.map((key, val) => {
-                        return (
-                            key.map((listValues, index) =>
-                                <tr key={index}>
-                                    <td style={{ "borderWidth": "1px", 'borderColor': "#aaaaaa", 'borderStyle': 'solid', 'textAlign': 'center' }}>{listValues.split("|")[0]}</td>
-                                    <td style={{ "borderWidth": "1px", 'borderColor': "#aaaaaa", 'borderStyle': 'solid', 'textAlign': 'center' }}>{listValues.split("|")[1]}</td>
+                    {
+                        Object.entries(finalData).map(([key,val]) => {
+                            return (
+                                <tr>
+                                         <td style={{ "borderWidth": "1px", 'borderColor': "#aaaaaa", 'borderStyle': 'solid', 'textAlign': 'center' }}>{(val[0] === undefined)?'':("000"+val[0].split("|")[0]).substring("000"+val[0].split("|")[0].length - 2)}</td>
+                                         <td style={{ "borderWidth": "1px", 'borderColor': "#aaaaaa", 'borderStyle': 'solid', 'textAlign': 'center' }}>{(val[0] === undefined)?'':val[0].split("|")[1]}</td>
+                                         <td style={{ "borderWidth": "1px", 'borderColor': "#aaaaaa", 'borderStyle': 'solid', 'textAlign': 'center' }}>{(val[1] === undefined)?'':("000"+val[1].split("|")[0]).substring("000"+val[1].split("|")[0].length - 2)}</td>
+                                         <td style={{ "borderWidth": "1px", 'borderColor': "#aaaaaa", 'borderStyle': 'solid', 'textAlign': 'center' }}>{(val[1] === undefined)?'':val[1].split("|")[1]}</td>
+                                         <td style={{ "borderWidth": "1px", 'borderColor': "#aaaaaa", 'borderStyle': 'solid', 'textAlign': 'center' }}>{(val[2] === undefined)?'':("000"+val[2].split("|")[0]).substring("000"+val[2].split("|")[0].length - 2)}</td>
+                                         <td style={{ "borderWidth": "1px", 'borderColor': "#aaaaaa", 'borderStyle': 'solid', 'textAlign': 'center' }}>{(val[2] === undefined)?'':val[2].split("|")[1]}</td>     
                                 </tr>
                             )
-                        )
-                    })}
+                        })
+                    }
                 </table>
             </Box>
 
@@ -113,7 +110,6 @@ export default function SimpleDialogDemo({ ticketData }) {
         setOpen(false);
     };
     const obj = JSON.parse(ticketData)
-    //console.log(typeof obj +" "+JSON.stringify(obj) +" "+ obj.draw);
     return (
         <div>
             <Button variant="contained" onClick={handleClickOpen} style={{ fontSize: '13px', padding: 5, margin: '1px' }}>
